8281238adca04b028e4ba43c0dd25213ab24d719
Prakhar Sharma <93972115+Prakhar077@users.noreply.github.com>
2023-07-22 17:40:21 +0530

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace corewithmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
